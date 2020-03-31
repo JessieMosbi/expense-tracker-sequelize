f8dc64500756dab81787b5a51b14aab7fa9aa4a3
JessieCheng <hamy820326@gmail.com>
2020-03-31 22:13:22 +0800

    install middleware related to views
    express-handlebars: template engine to deal with views
    body-parser: deal with HTTP POST method data
    method-override: to override HTTP GET POST method to PUT DELETE (more RESTful route)

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
 const app = express()
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+// route
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.delete('/', (req, res) => {
+  console.log('delete index')
+})
 
 app.listen(3000, (req, res) => {
   console.log('Server start listening port 3000')
