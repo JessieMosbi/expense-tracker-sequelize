6e904c680ce75092b8d35883f4428392cec33e78
JessieCheng <hamy820326@gmail.com>
2020-04-05 14:47:31 +0800

    finished register page
    install bcryptjs to hash password
    install connect-flash to manage message

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 // model
 const db = require('../models')
@@ -11,7 +12,49 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.send('註冊')
+  const { name, email, password, password2 } = req.body
+  const errors = []
+
+  // check value
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: '所有欄位均為必填' })
+  }
+  if (password !== password2) {
+    errors.push({ message: '兩次密碼不一致' })
+  }
+  if (errors.length > 0) {
+    res.render('register', { name, email, password, password2, errors })
+    return false
+  }
+
+  // 檢查 user 是否存在
+  User.findOne({ where: { email } })
+    .then((user) => {
+      if (user) {
+        console.log('User already exists')
+        res.render('register', {
+          name,
+          email,
+          password,
+          password2
+        })
+      } else {
+        // insert into DB
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) console.log(err)
+          bcrypt.hash(req.body.password, salt, (err, hash) => {
+            if (err) console.log(err)
+            User.create({
+              name,
+              email,
+              password: hash
+            })
+              .then((user) => res.redirect('/'))
+              .catch((err) => console.log(err))
+          })
+        })
+      }
+    })
 })
 
 // 登入
