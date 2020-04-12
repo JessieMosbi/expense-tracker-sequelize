22a7fcb8ae513adc151de8248c67e3e7c740d3fd
JessieCheng <hamy820326@gmail.com>
2020-04-12 21:08:34 +0800

    add default value for dateFormat func in lib js

--- config/lib.js
+++ config/lib.js
@@ -1,5 +1,5 @@
 module.exports = {
-  dateFormat: (date, splitChar) => {
+  dateFormat: (date, splitChar = '-') => {
     const year = date.getFullYear()
     const month = ('0' + (date.getMonth() + 1)).substr(-2)
     const day = ('0' + date.getDate()).substr(-2)
